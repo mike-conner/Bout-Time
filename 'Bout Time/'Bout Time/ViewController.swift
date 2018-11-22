9e3c084f6e3e74804e451c37496b12b0d2f66985
Mike Conner <connerland@yahoo.com>
2018-11-22 12:38:21 -0500

    Rounded Corners
    Rounded the corners of the question labels.

--- 'Bout Time/'Bout Time/ViewController.swift
+++ 'Bout Time/'Bout Time/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionOneLabel: UILabel!
+    @IBOutlet weak var questionTwoLabel: UILabel!
+    @IBOutlet weak var questionThreeLabel: UILabel!
+    @IBOutlet weak var questionFourLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        questionOneLabel.clipsToBounds = true
+        questionOneLabel.layer.cornerRadius = 3
+        questionTwoLabel.clipsToBounds = true
+        questionTwoLabel.layer.cornerRadius = 3
+        questionThreeLabel.clipsToBounds = true
+        questionThreeLabel.layer.cornerRadius = 3
+        questionFourLabel.clipsToBounds = true
+        questionFourLabel.layer.cornerRadius = 3        
     }
 
 
