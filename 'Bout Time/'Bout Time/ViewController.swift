646995a64ee90fec18de57b8940a5d2ed936bdfa
Mike Conner <connerland@yahoo.com>
2018-11-22 09:41:26 -0500

    Created Project

---
+++ 'Bout Time/'Bout Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  'Bout Time
+//
+//  Created by Mike Conner on 11/22/18.
+//  Copyright © 2018 Mike Conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 'Bout Time/'Bout TimeTests/_Bout_TimeTests.swift
@@ -0,0 +1,34 @@
+//
+//  _Bout_TimeTests.swift
+//  'Bout TimeTests
+//
+//  Created by Mike Conner on 11/22/18.
+//  Copyright © 2018 Mike Conner. All rights reserved.
+//
+
+import XCTest
+@testable import _Bout_Time
+
+class _Bout_TimeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
